be461d92c20b3d6ce736c6814207b3895ce57fa1
Valeriy <valeriy.elykomov@gmail.com>
2024-01-03 00:59:01 +0500

    Rename configuration extensions

--- Extensions/ConfigurationExtensions.cs
+++ Extensions/ConfigurationExtensions.cs
@@ -3,19 +3,13 @@ using TelegramBot_OpenAI.Configurations;
 
 namespace TelegramBot_OpenAI.Extensions
 {
-    public static class WebAppConfigurationExtensions
+    public static class ConfigurationExtensions
     {
-        /// <summary>
-        /// Shorthand for GetSection(OpenAIToken)[value].
-        /// </summary>
-        /// <param name="configuration"></param>
-        /// <returns>The Open AI token.</returns>
-        /// <exception cref="ConfigurationErrorsException"></exception>
         public static string GetOpenAIToken(this IConfiguration configuration)
         {
             var openAIConfiguration = configuration.GetSection(SecretsConfiguration.OpenAiConfiguration)
                                                    .Get<OpenAiConfiguration>()
-                                                   ?? throw new ConfigurationErrorsException("Ope");
+                                                   ?? throw new ConfigurationErrorsException("Open AI TOKEN NOT CONTAINS IN JSON");
 
             return openAIConfiguration.MyToken;
         }
